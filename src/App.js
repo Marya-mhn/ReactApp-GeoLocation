2e247e2ea7c3a66c365cfee1630f30e4111cdd66
Maghy <105222649+MaryaMhn2001@users.noreply.github.com>
2023-03-19 13:53:30 +0100

    The user location is detected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
+import { useState } from "react";
+import React from "react";
+import { render } from "@testing-library/react";
+import L from "leaflet";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const icon = L.icon({
+  iconUrl:
+    "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png",
+  iconSize: [48, 48],
+  iconAnchor: [24, 48],
+  popupAnchor: [0, -48],
+});
+
+function LocationMarker() {
+  const [position, setPosition] = useState(null);
+
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position} icon={icon}>
+      <Popup>You are here</Popup>
+    </Marker>
   );
 }
 
-export default App;
+render(
+  <MapContainer
+    center={{ lat: 51.505, lng: -0.09 }}
+    zoom={13}
+    scrollWheelZoom={false}
+  >
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <LocationMarker />
+  </MapContainer>
+);
+export default LocationMarker;
